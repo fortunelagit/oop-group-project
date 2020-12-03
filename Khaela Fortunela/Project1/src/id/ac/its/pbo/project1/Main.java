438f2741204b677f8034827b91c7dbfe4bd9e310
fortunelagit <72397657+fortunelagit@users.noreply.github.com>
2020-12-03 12:17:56 +0700

    Update Main.java

--- Khaela Fortunela/Project1/src/id/ac/its/pbo/project1/Main.java
+++ Khaela Fortunela/Project1/src/id/ac/its/pbo/project1/Main.java
@@ -5,6 +5,18 @@ import javax.swing.JOptionPane;
 public class Main {
 
 	public static void main(String[] args) {
+		//String[] options = {"Circle", "Triangle", "Rectangle", "Square"};
+		//int choice = JOptionPane.showOptionDialog(null, "Choose any shape to calculate.",
+	        //        "Click a button",
+	        //        JOptionPane.DEFAULT_OPTION, 
+	        //        JOptionPane.INFORMATION_MESSAGE, null, 
+	        //        options, options[0]);
+		//
+		//switch(choice) {
+		//	case 0:	Circle.Application();
+		//			break;
+		//}
+		
 		JOptionPane.showMessageDialog(null, "Welcome to Circle Calculator!!");
 		int radius = Integer.parseInt(JOptionPane.showInputDialog("Enter radius of circle:"));
 		Circle c = new Circle(radius);
