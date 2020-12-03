d0c178954f602e480e350f9a0ec87432da6cb219
fortunelagit <72397657+fortunelagit@users.noreply.github.com>
2020-12-03 15:45:22 +0700

    Add files via upload

---
+++ Khaela Fortunela/Project2/src/id/ac/its/pbo/project2/LabelTest.java
@@ -0,0 +1,14 @@
+package id.ac.its.pbo.project2;
+
+import javax.swing.JFrame;
+
+public class LabelTest
+{
+   public static void main(String[] args)
+   { 
+      LabelFrame labelFrame = new LabelFrame(); 
+      labelFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      labelFrame.setSize(300, 200); 
+      labelFrame.setVisible(true);; 
+   } 
+} // end class LabelTest
